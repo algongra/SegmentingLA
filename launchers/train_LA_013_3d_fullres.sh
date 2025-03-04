35c87e9ee1b2ba778c2f9ab967feac4f2f6b6800
Yvonne Stöcker <yvonne.stoecker@uwien.ac.at>
2025-03-04 10:21:18 +0100

    New option to train on specified GPU and ifold

--- launchers/train_LA_013_3d_fullres.sh
+++ launchers/train_LA_013_3d_fullres.sh
@@ -20,10 +20,20 @@ KFOLD=5
 TR_CONFIG=3d_fullres
 PLAN_TYPE=nnUNetResEncUNetLPlans
 
+# optional: select the GPU to run on
+#export CUDA_VISIBLE_DEVICES=6
+# optional: declare which fold to run
+#IFOLD=0
 
-# Call function train_fun.sh for each fold
-for IFOLD in $( seq 0 $(($KFOLD-1)) )
- do
-    train_fun $nnUNet_results $DATASET_ID $DATASET_NAME $IFOLD $TR_CONFIG \
-              $PLAN_TYPE
- done
+# if IFOLD is not defined, call function train_fun.sh for each fold
+# otherwise, only train on this fold
+if [[ -z "${IFOLD}" ]]; then
+ for IFOLD in $( seq 0 $(($KFOLD-1)) )
+  do
+     train_fun $nnUNet_results $DATASET_ID $DATASET_NAME $IFOLD $TR_CONFIG \
+               $PLAN_TYPE
+  done
+else
+ train_fun $nnUNet_results $DATASET_ID $DATASET_NAME $IFOLD $TR_CONFIG \
+           $PLAN_TYPE
+fi
